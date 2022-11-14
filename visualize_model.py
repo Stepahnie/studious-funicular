461643bda2b59c59b8a7ac663253e0de27777003
Stephanie Ekekwe <stephanie@mono.co>
2022-11-14 16:28:38 +0100

    changes made

--- visualize_model.py
+++ visualize_model.py
@@ -3,13 +3,8 @@ import typer
 
 
 def main():
-    models = [name.strip() for name in models.split(",")]
+    models = ["./model/credit-trans-classifier/model-best/", "./model/debit-trans-classifier/model-best/"]
     default_text = "MTHLY SALARY via GAPS JUNE Salary from PIGGYTECH GLOBAL LIMITED to IFEZUE SOMTOCHUKWU DAVID REF: ,credit,82900000,2020-06-26T00:00:00Z"
     spacy_streamlit.visualize(models, default_text, visualizers=["textcat"])
 
 
-if __name__ == "__main__":
-    try:
-        typer.run(main)
-    except SystemExit:
-        pass
