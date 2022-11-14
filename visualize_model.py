c4ad1e3a8976e08f10b06f0509b6b87a7ebd87a7
Stephanie Ekekwe <stephanie@mono.co>
2022-11-14 16:14:55 +0100

    intial commit

---
+++ visualize_model.py
@@ -0,0 +1,15 @@
+import spacy_streamlit
+import typer
+
+
+def main(models: str):
+    models = [name.strip() for name in models.split(",")]
+    default_text = "MTHLY SALARY via GAPS JUNE Salary from PIGGYTECH GLOBAL LIMITED to IFEZUE SOMTOCHUKWU DAVID REF: ,credit,82900000,2020-06-26T00:00:00Z"
+    spacy_streamlit.visualize(models, default_text, visualizers=["textcat"])
+
+
+if __name__ == "__main__":
+    try:
+        typer.run(main)
+    except SystemExit:
+        pass
