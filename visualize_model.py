857d2c5c44b306c7b67c5961cd53ee2e52776c28
Stephanie Ekekwe <stephanie@mono.co>
2022-11-14 17:20:48 +0100

    changes made

--- visualize_model.py
+++ visualize_model.py
@@ -5,7 +5,10 @@ import spacy_streamlit  as sst
 
 
 def main():
-    models = ["model/credit-trans-classifier/model-best/", "model/debit-trans-classifier/model-best/"]
+    st.title("Mono - Transaction Narration Classifiaction")
+    st.sidebar.title("Select Transaction Model Type")
+
+    models = ["model/credit-trans-classifier/model-best/", "model/debit-trans-classifier/model-best"]
     default_text = "MTHLY SALARY via GAPS JUNE Salary from PIGGYTECH GLOBAL LIMITED to IFEZUE SOMTOCHUKWU DAVID REF: ,credit,82900000,2020-06-26T00:00:00Z"
     sst.visualize(models, default_text, visualizers=["textcat"])
 
