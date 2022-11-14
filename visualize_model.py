1900129ca7a1603ac4bb213506d7d8547fa6c36b
Stephanie Ekekwe <stephanie@mono.co>
2022-11-14 16:44:39 +0100

    changes made

--- visualize_model.py
+++ visualize_model.py
@@ -3,7 +3,7 @@ import typer
 
 
 def main():
-    models = ["./model/credit-trans-classifier/model-best/", "./model/debit-trans-classifier/model-best/"]
+    models = ["model/credit-trans-classifier/model-best/", "model/debit-trans-classifier/model-best/"]
     default_text = "MTHLY SALARY via GAPS JUNE Salary from PIGGYTECH GLOBAL LIMITED to IFEZUE SOMTOCHUKWU DAVID REF: ,credit,82900000,2020-06-26T00:00:00Z"
     spacy_streamlit.visualize(models, default_text, visualizers=["textcat"])
 
