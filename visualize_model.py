0e59ed8bd01e603d152bf8f1989cb7b5e8015ac3
Stephanie Ekekwe <stephanie@mono.co>
2022-11-14 17:06:18 +0100

    change made

--- visualize_model.py
+++ visualize_model.py
@@ -1,13 +1,13 @@
 # Core Pkgs
 import streamlit as st 
 import streamlit.components.v1 as stc 
-# import spacy_streamlit
+import spacy_streamlit  as sst
 
 
 def main():
     models = ["model/credit-trans-classifier/model-best/", "model/debit-trans-classifier/model-best/"]
     default_text = "MTHLY SALARY via GAPS JUNE Salary from PIGGYTECH GLOBAL LIMITED to IFEZUE SOMTOCHUKWU DAVID REF: ,credit,82900000,2020-06-26T00:00:00Z"
-    st.visualize(models, default_text, visualizers=["textcat"])
+    sst.visualize(models, default_text, visualizers=["textcat"])
 
 
 
